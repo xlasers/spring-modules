b5e3a92ffa684ae4df826565ae01b052f8c4659d
Elijah <withxlaser@gmail.com>
2020-03-22 12:23:32 +0800

    :art: improve eureka demo structure

--- spring-cloud/eureka/eureka-server/src/main/java/com/xlaser4j/eureka/EurekaServerApplication.java
+++ spring-cloud/eureka/eureka-server/src/main/java/com/xlaser4j/eureka/EurekaServerApplication.java
@@ -13,11 +13,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
         System.out.println("\n================================ Eureka是Netflix开源的服务发现注册组件,但是2018年闭源停更,老项目可能仍在使用,这里简单了解使用 =================================\n");
         System.out.println("\n================================ 注册中心是核心组件,这里通过修改本地hosts文件,创建了域名eurekaA,eurekaB,通过jar包启动模拟集群环境 =================================\n");
         System.out.println("\n================================ java -jar eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=eurekaA =================================\n");
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
