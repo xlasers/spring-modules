9747e562bbd93f67e0e1a36422513e475f0b3df0
Elijah <withxlaser@gmail.com>
2020-04-01 21:28:18 +0800

    :sparkles: completed spring-cloud-gateway demos

---
+++ spring-cloud/gateway/gateway-proxy/src/main/java/com/xlaser4j/gateway/GateWayProxyApplication.java
@@ -0,0 +1,16 @@
+package com.xlaser4j.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @package: com.xlaser4j.gateway
+ * @author: Elijah.D
+ * @time: 2020/4/1 21:25
+ * @description:
+ * @modified: Elijah.D
+ */
+public class GateWayProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayProxyApplication.class);
+    }
+}
